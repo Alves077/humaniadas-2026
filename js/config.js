0750685880dc87eccb3c69c45a1b0208d9106cb8
Alves077 <alves_carlos@id.uff.br>
2026-07-07 20:05:23 -0300

    chore: remove arquivos ignorados do rastreamento git
    config.js (chaves Supabase) e SESSION_CHECKPOINT.md estavam no .gitignore
    mas continuavam sendo rastreados por já terem sido commitados anteriormente.
    git rm --cached remove do índice sem deletar localmente.

--- js/config.js
+++
@@ -1,4 +0,0 @@
-const SUPABASE_URL = 'https://jjmopgirtmtkhzbtogqa.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_UhTHX1rTOl3onu1YWs41lQ_iKvOK-g2';
-
-const CHAMPIONSHIP_ID = 'humaniadas-2026';
